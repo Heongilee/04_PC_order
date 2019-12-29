4199925fcf6c5fca4bd8b9b6504b254b5f866006
HeongiLee <gjsrl1@gmail.com>
2019-12-29 17:26:36 +0900

    Interface Define

--- PCorder/src/main/java/View/MyFrame.java
+++ PCorder/src/main/java/View/MyFrame.java
@@ -2,22 +2,45 @@ package View;
 
 import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.concurrent.Flow;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
+import Controller.Login;
+
 public class MyFrame extends JFrame{
-	private JLabel l = new JLabel("TEST");
+	private JButton btn = new JButton("test");
 	public MyFrame() {
 		super("MVC Modeling");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container c = getContentPane();
 		c.setLayout(new FlowLayout());
 		
-		c.add(l);
+		btn.addActionListener(new AL());
+		c.add(btn);
 		
 		setSize(300, 300);
 		setVisible(true);
+	}
+	public class AL implements ActionListener, Login{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			JButton b = (JButton)e.getSource();
+			if(b.getText().equals("test")) {
+				System.out.println("[1] : 정상 작동");
+				this.Submit();
+			}
+		}
+		@Override
+		public void Submit() {
+			System.out.println("[2] : 정상 작동");
+		}
+		@Override
+		public void Register() {
+		}
 	}
 }
