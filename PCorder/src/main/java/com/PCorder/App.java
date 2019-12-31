11a081b845b4436cc0eca91ad4a7147f84daa735
HeongiLee <gjsrl1@gmail.com>
2019-12-31 15:46:56 +0900

    191231 15:46

--- PCorder/src/main/java/com/PCorder/App.java
+++ PCorder/src/main/java/com/PCorder/App.java
@@ -11,8 +11,8 @@ public class App
     public static void main( String[] args) throws Exception{
     	new LoginView(null);
     	//Customers_DAO.getInstance(); //해줘도 되고 안 해줘도 됨. 바로 메모리 할당 해버리기 때문.
-//    	Customers_DAO.getConnection();
-//    	Customers_DAO.CUSTOMERS_FUNC_1();
-//    	Customers_DAO.closeJDBC(Customers_DAO.conn, Customers_DAO.pstmt, Customers_DAO.stmt, Customers_DAO.rs);
+    	Customers_DAO.getConnection();
+    	Customers_DAO.CUSTOMERS_FUNC_1();
+    	Customers_DAO.closeJDBC(Customers_DAO.conn, Customers_DAO.pstmt, Customers_DAO.stmt, Customers_DAO.rs);
     }
 }
